b270e60a978e493def21d57dd88e54c40c43fa8f
kevindoni <kevindoni17@gmail.com>
2021-12-18 09:53:22 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear
+echo -e "========================================"
+echo -e "#                 Premium Auto Script Kevindoni                    #"
+echo -e "#-------------------------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit                             #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit                      #"
+echo -e "# For VPS with KVM and VMWare virtualization                  #"
+echo -e "#-------------------------------------------------------------#"
+echo -e "# Rebuild By Kevindoni                                             #"
+echo -e "# Contact : t.me                                         #"
+echo -e "========================================"
