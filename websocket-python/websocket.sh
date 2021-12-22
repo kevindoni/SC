90044aabb73aa79596687782ec7af20d019ee3dd
kevindoni <kevindoni17@gmail.com>
2021-12-22 20:17:56 +0700

    Update websocket.sh
    main

--- websocket-python/websocket.sh
+++ websocket-python/websocket.sh
@@ -24,8 +24,6 @@ wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/${
 
 #Install Script Websocket-SSH Python
 cd
-mkdir /home/admin/
-cd
 cd /usr/local/bin/
 wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/${GitUser}/SC/main/${namafolder}/ws-openssh && chmod +x /home/vps/ws-openssh
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/${GitUser}/SC/main/${namafolder}/ws-dropbear && chmod +x /home/vps/bin/ws-dropbear
